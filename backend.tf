ca0968b10cef028658d1636317a87b807b054867
junkit22 <junkit22@gmail.com>
2024-09-11 22:09:03 +0800

    update region

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
   backend "s3" {
     bucket = "sctp-ce7-tfstate"
     key    = "junjie-tf-workspace-act.tfstate"   #Change the value of this to yourname-tf-workspace-act.tfstate for  example
-    region = "ap-southeast-1"
+    region = "us-east-1"
   }
 }
